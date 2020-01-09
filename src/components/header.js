5098f5645288aeb26ecc09726816930a3221731c
loyd0 <samloyd@hotmail.co.uk>
2020-01-09 13:04:32 +0000

    added logo from contenful

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,64 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { graphql, useStaticQuery } from 'gatsby'
+import {Img} from 'gatsby-image'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+const Header = ({ siteTitle }) => {
+
+  const data = useStaticQuery(graphql`
+    query Logos {
+      allContentfulLogo {
+        edges {
+          node {
+            id
+            logo {
+              file {
+                url
+                fileName
+                contentType
+              }
+            }
+            name
+          }
+        }
+      }
+    }
+  `)
+
+
+  console.log(data)
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+        <img src={data.allContentfulLogo.edges[1].node.logo.file.url}/>
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </div>
+    </header>
+  )
+
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
@@ -38,5 +67,7 @@ Header.propTypes = {
 Header.defaultProps = {
   siteTitle: ``,
 }
+
+
 
 export default Header
