e1941ec639eb586f920926c0d7100e5cd99f6751
loyd0 <samloyd@hotmail.co.uk>
2020-02-05 17:35:00 +0000

    added in local version of infinite react carousel due to build time errors (no Element and no document in node env)

--- src/components/Elements/Carousel.js
+++ src/components/Elements/Carousel.js
@@ -2,22 +2,30 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Img from 'gatsby-image'
 // https://github.com/g787543/infinite-react-carousel
-import Slider from 'infinite-react-carousel';
+// import Slider from 'infinite-react-carousel';
 
 const Carousel = ({ images }) => {
 
     const imagesArray = images.map(image => image.fluid)
+
+    const NextArrow = (props)=> <div {...props} ><i className="fa  fa-chevron-right text-4xl text-secondary hover:text-primary hover:cursor-pointer" /></div>
+    const PrevArrow = (props)=> <div {...props}><i className="fa fa-chevron-left text-4xl text-secondary hover:text-primary hover:cursor-pointer" /></div>
+
+
     return (
-        <Slider
-            className="w-full h-full"
-            dots={true}
-            nextArrow={<div><i className="fa fa-chevron-right text-4xl text-secondary hover:text-primary hover:cursor-pointer" /></div>}
-            prevArrow={<div><i className="fa fa-chevron-left text-4xl text-secondary hover:text-primary hover:cursor-pointer" /></div>}
-            dotsClass="z-10 carousel-dots relative bottom-0" >
-            {imagesArray.map(image => <div className="w-full h-full">
-                <Img className="h-50vh md:h-screen z-0 " fluid={image} />
-            </div>)}
-        </Slider>
+        // <Slider
+        //     arrows={false}
+        //     className="w-full h-full"
+        //     dots={true}
+        //     // nextArrow={<NextArrow />}
+        //     // prevArrow={<PrevArrow />}
+        //     dotsClass="z-10 carousel-dots relative bottom-0" >
+        //     {imagesArray.map((image, index) => <div key={index} className="w-full h-full">
+        //         <Img className="h-50vh md:h-screen z-0 " fluid={image} />
+        //     </div>)}
+        // </Slider>
+        <>
+        </>
     );
 }
 
--- src/components/Sections/Carousel.js
+++ src/components/Sections/Carousel.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react'
-import CarouselNavigation from '../Elements/CarouselNavigation'
 // https://github.com/g787543/infinite-react-carousel
-import Slider from 'infinite-react-carousel';
+import Slider from '../../infinite-react-carousel';
 
 
 const Content = ({ content }) => {
   return (
     <div
-      className="w-screen z-10 w-full text-center w-full px-4 py-6 max-w-3xl mx-auto sm:py-12 bg-blue-400 flex-col flex justify-center"
+      className="z-10  text-center px-4 py-6 max-w-3xl mx-auto sm:py-12 bg-blue-400 flex-col flex justify-center"
       style={{
         minHeight: 400,
         maxHeight: '100vh',
@@ -15,32 +14,37 @@ const Content = ({ content }) => {
       }}
     >
       <p className="text-center text-xl ">"{content.description.description}"</p>
-      <p className="text-center text-lg italic ">{content.title}
-      </p>
+      <p className="text-center text-lg italic ">{content.title} </p>
     </div>
   )
 }
+
+const NextArrow = (props)=> <div {...props} ><i className="fa  fa-chevron-right text-4xl text-secondary hover:text-primary hover:cursor-pointer" /></div>
+// const NextArrow = ()=> <div>{"HI"}</div>
+const PrevArrow = (props)=> <div {...props}><i className="fa fa-chevron-left text-4xl text-secondary hover:text-primary hover:cursor-pointer" /></div>
+// const PrevArrow = ()=> <div>{"BYE"}</div>
 
 const ContentContainer = ({ content }) => {
   const [active, setActive] = useState(0)
 
 
   return (
-    <Slider 
-    nextArrow={<div><i className="fa fa-chevron-right text-4xl text-secondary hover:text-primary hover:cursor-pointer" /></div>}
-    prevArrow={<div><i className="fa fa-chevron-left text-4xl text-secondary hover:text-primary hover:cursor-pointer" /></div>}
-    dotsClass="z-10 carousel-dots relative bottom-0" 
-    className="w-screen h-screen w-100 z-10" dots>
-      {content.map(cont =>
-        <div>
+ <Slider 
+      dotsClass="z-10 carousel-dots relative bottom-0"             
+      className="w-screen min-h-screen z-10"
+      dots
+      // nextArrow={<NextArrow />}
+      // prevArrow={<PrevArrow />}
+    >
+      {content.map((cont, index) =>
+        <div key={cont.title + index}>
           <div className="h-screen flex flex-col justify-center ">
             <Content content={cont} />
           </div>
-
         </div>
-
       )}
     </Slider>
+   
   )
 }
 
